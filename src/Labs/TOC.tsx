edacff8850efb88f743019e9d9e5d0724fe55b86
nickmamisashvili <mamisashvili.n@northeastern.edu>
2024-10-21 17:06:33 -0400

    added github link and name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,48 +4,65 @@ export default function TOC() {
   const { pathname } = useLocation();
 
   return (
-    <ul className="nav nav-pills">
-      <li className="nav-item">
-        <Link className="nav-link" id="wd-a" to="/Labs">
-          Labs {pathname}
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
-          id="wd-a1"
-          to="/Labs/Lab1"
-        >
-          Lab 1
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
-          id="wd-a2"
-          to="/Labs/Lab2"
-        >
-          Lab 2
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
-          id="wd-a3"
-          to="/Labs/Lab3"
-        >
-          Lab 3
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          className={`nav-link ${pathname.includes("Kanbas") ? "active" : ""}`}
-          id="wd-k"
-          to="/Kanbas"
-        >
-          Kanbas
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <Link className="nav-link" id="wd-a" to="/Labs">
+            Labs {pathname}
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
+            id="wd-a1"
+            to="/Labs/Lab1"
+          >
+            Lab 1
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
+            id="wd-a2"
+            to="/Labs/Lab2"
+          >
+            Lab 2
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
+            id="wd-a3"
+            to="/Labs/Lab3"
+          >
+            Lab 3
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className={`nav-link ${
+              pathname.includes("Kanbas") ? "active" : ""
+            }`}
+            id="wd-k"
+            to="/Kanbas"
+          >
+            Kanbas
+          </Link>
+        </li>
+        <i></i>
+        <h1> hi</h1>
+      </ul>
+      <ul>
+        <li>
+          <h1>Nicholas Mamisashvili</h1>
+          <Link
+            id="wd-github"
+            to={"https://github.com/Nickdaco/kanbas-react-web-app/tree/main"}
+          >
+            https://github.com/Nickdaco/kanbas-react-web-app/tree/main
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
